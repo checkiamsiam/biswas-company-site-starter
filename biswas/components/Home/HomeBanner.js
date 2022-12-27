89ee2220c43abd2eec91888d64a3967c8456bf5d
checkiamsiam <issiam02415@gmail.com>
2022-12-27 23:04:07 +0600

    git banner responsive

--- biswas/components/Home/HomeBanner.js
+++ biswas/components/Home/HomeBanner.js
@@ -1,14 +1,15 @@
-import { Container, Stack, Typography, useTheme } from "@mui/material";
+import { Container, Link, Stack, Typography, useTheme } from "@mui/material";
 import Image from "next/image";
 import style from "../../styles/home.module.css";
 import logo from "../../assets/images/My project.png";
 import MyButton from "../common/MyButton";
+import NextLink from "next/link";
 
 const HomeBanner = () => {
   const theme = useTheme();
   return (
-    <Container maxWidth="xl" className={style.setBg} >
-      <Stack direction="row" justifyContent="center" alignItems="center" minHeight="100vh"  sx={{ paddingTop: "150px" }}>
+    <Container maxWidth="xl" className={style.setBg}>
+      <Stack direction="row" justifyContent="center" alignItems="center" minHeight="100vh" sx={{ paddingTop: "150px" }}>
         <Stack width={{ lg: "50%", xs: "100%" }}>
           <Stack direction="row" justifyContent="center">
             <Image src={logo} height="100" width="100" alt="logo" />
@@ -20,7 +21,9 @@ const HomeBanner = () => {
             Modern Technologies and Real-Time Support
           </Typography>
           <Stack direction="row" justifyContent="center" marginTop="20px">
-            <MyButton>Know More</MyButton>
+            <Link href="#" component={NextLink} underline="none">
+              <MyButton>Know More</MyButton>
+            </Link>
           </Stack>
         </Stack>
       </Stack>
